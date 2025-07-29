8ccee781ac49b580816017072ea9aaeb3ddfa74d
Agustin G <agusgibaut693@gmail.com>
2025-07-29 19:14:03 -0300

    primera parte/ejercicios

---
+++ ejercicios-01/variables.js
@@ -0,0 +1,7 @@
+//variable nombre
+let nombre = "agustin";
+//variable apellido
+let apellido = "Gibaut";
+
+//mostrando por consola los datos de la variable nombre + apellido
+console.log(nombre, apellido)
---
+++ variables.js
@@ -0,0 +1,14 @@
+/*
+Declara una variable llamada nombre en tu consola y asignale tu nombre.
+Declara una variable llamada apellido y asignale como valor tu apellido.
+Muestra por consola las variables nombre y apellido.
+*/
+
+
+//variable nombre
+let nombre = prompt ((`ingrese su nombre, por vafor:`))
+//variable apellido
+let apellido = prompt ((`ingrese su apellido, por favor`))
+
+//mensaje de bienvenida con concatenación de la variable nombre + apellido
+alert(`Bienvenid@ a nuestra página ${nombre} ${apellido} !`)
